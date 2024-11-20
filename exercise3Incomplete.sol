a0e32b33b22225be4bbe77314a165094762df7b3
Ian Mitchell <56634530+iangmitchell@users.noreply.github.com>
2024-11-20 16:58:29 +0000

    Update exercise3Incomplete.sol

--- exercise3Incomplete.sol
+++ exercise3Incomplete.sol
@@ -32,12 +32,3 @@ contract caller2 {
         } 
 }          
 
-/*
-Completely incorrect 
- - callIncrement is fine and works
- - delegatecall doesn't since the context has switched to caller contract and it increments slot 0, 
- which is the calledAddress. This subsequently fails the rest of the code.
- */
- 
-         //insert the line below at line 14 to make the callerCounter the slot 0 and not the calledAddress
-        //uint public callerCounter;
